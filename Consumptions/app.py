6707b6e24cb813c8e1089c26affa2842484b0ca1
Warthula Dewpura <99714414+IT21254420@users.noreply.github.com>
2025-01-08 22:50:21 +0530

    Update app.py

--- Consumptions/app.py
+++ Consumptions/app.py
@@ -1,21 +1,24 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import json
 from datetime import datetime
+from google.oauth2.service_account import Credentials
 
 # Load the trained models
-electricity_model = pickle.load(open('electricity_pkl.sav', 'rb'))
-steam_model = pickle.load(open('steam_pkl.sav', 'rb'))
-water_model = pickle.load(open('water_pkl.sav', 'rb'))
+electricity_model = pickle.load(open('Consumptions/electricity_pkl.sav', 'rb'))
+steam_model = pickle.load(open('Consumptions/steam_pkl.sav', 'rb'))
+water_model = pickle.load(open('Consumptions/water_pkl.sav', 'rb'))
 
 # Google Sheets setup
 def save_to_google_sheet(data):
-    # Define the scope and authenticate
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name('consumption-prediction-f7496e8abee0.json', scope)
-    client = gspread.authorize(creds)
+    # Load the service account key from Streamlit secrets
+    service_account_info = json.loads(st.secrets["google"]["GOOGLE_CLOUD_KEY"])
+    credentials = Credentials.from_service_account_info(service_account_info)
+
+    # Authorize with Google Sheets
+    import gspread
+    client = gspread.authorize(credentials)
 
     # Open the spreadsheet
     sheet = client.open("Predictions Data").sheet1
